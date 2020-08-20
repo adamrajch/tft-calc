0ceb37b9d412ed38d496c5fb067f3ca198ad4db3
adamrajch <arajchwald@gmail.com>
2020-08-20 03:59:36 -0500

    mhm

--- components/Form.js
+++ components/Form.js
@@ -1,6 +1,33 @@
-import { Grid, Form } from "semantic-ui-react";
+import { Grid, Form, Input, Icon, Button } from "semantic-ui-react";
+import { useState } from "react";
+function TFTForm() {
+  const [champ, setChamp] = useState("");
+  return (
+    <Grid textAlign="center" columns={2} container>
+      <Grid.Row>
+        <Grid.Column width={6}>
+          <Input
+            placeholder="Search unit..."
+            value={champ}
+            onChange={(e) => setChamp(e.target.value)}
+          >
+            <input />
+            <Button
+              icon="search"
+              onClick={() => console.log(champ)}
+              color="green"
+            />
+          </Input>
+        </Grid.Column>
+        <Grid.Column width={6}>
+          <Form></Form>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <div>Display Specs</div>
+      </Grid.Row>
+    </Grid>
+  );
+}
 
-const Form = () => {
-  return <></>;
-};
-export default Form;
+export default TFTForm;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header";
+import Form from "../components/Form";
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>TFT Calc</title>
       </Head>
-
-      <main className={styles.main}>
-        <Header />
-      </main>
+      <Header />
+      <div className={styles.content}>
+        <Form />
+      </div>
 
       <footer className={styles.footer}>
         <span>Rajchwald Bros</span>

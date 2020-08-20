3ee1f68c3d2236f74ec86d7b492c7d2b8b939628
adamrajch <arajchwald@gmail.com>
2020-08-20 02:24:07 -0500

    formatting

---
+++ components/Form.js
@@ -0,0 +1,6 @@
+import { Grid, Form } from "semantic-ui-react";
+
+const Form = () => {
+  return <></>;
+};
+export default Form;
---
+++ components/Header.js
@@ -0,0 +1,8 @@
+import { Icon, Header } from "semantic-ui-react";
+export default function Title() {
+  return (
+    <Header as="h1">
+      Team Fight Tactics Calculator <Icon name="calculator" size="big" />
+    </Header>
+  );
+}

405d78771ee941de6f39ac27f72ca24503508eb2
adamrajch <arajchwald@gmail.com>
2020-08-21 02:32:45 -0500

    changes

--- components/Form.js
+++ components/Form.js
@@ -1,7 +1,17 @@
-import { Grid, Form, Input, Icon, Button } from "semantic-ui-react";
+import {
+  Grid,
+  Form,
+  Input,
+  Icon,
+  Button,
+  Header,
+  Label,
+} from "semantic-ui-react";
 import { useState } from "react";
 function TFTForm() {
   const [champ, setChamp] = useState("");
+  const [lvl, setlvl] = useState(1);
+  // const
   return (
     <Grid textAlign="center" columns={2} container>
       <Grid.Row>
@@ -20,11 +30,26 @@ function TFTForm() {
           </Input>
         </Grid.Column>
         <Grid.Column width={6}>
-          <Form></Form>
+          <div>
+            Player Level: {lvl}{" "}
+            <Icon name="angle up" color="yellow" onClick={} />
+            <Icon name="angle down" color="yellow" />
+          </div>
+          <div>Unit Taken: </div>
+          <div>
+            Gold: <Icon name="angle up" color="yellow" />
+            <Icon name="angle down" color="yellow" />
+          </div>
+          <div>
+            Copies needed: <Icon name="angle up" color="yellow" />
+            <Icon name="angle down" color="yellow" />
+          </div>
         </Grid.Column>
       </Grid.Row>
       <Grid.Row>
-        <div>Display Specs</div>
+        <Grid.Row>
+          <Header as="h2">Specs</Header>
+        </Grid.Row>
       </Grid.Row>
     </Grid>
   );

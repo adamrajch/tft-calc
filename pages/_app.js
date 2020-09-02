24cbedd0e2aa45155009382219f9c0df53e5ebf7
adamrajch <arajchwald@gmail.com>
2020-09-02 17:03:36 -0500

    changed app

--- pages/_app.js
+++ pages/_app.js
@@ -3,17 +3,21 @@ import "semantic-ui-css/semantic.min.css";
 import Nav from "../components/Nav";
 import App from "next/app";
 import styles from "../styles/Home.module.css";
-class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-    return { pageProps };
-  }
-  render() {
-    const { Component, pageProps } = this.props;
-    return <Component {...pageProps} />;
-  }
+// class MyApp extends App {
+// static async getInitialProps({ Component, ctx }) {
+//   let pageProps = {};
+//   if (Component.getInitialProps) {
+//     pageProps = await Component.getInitialProps(ctx);
+//   }
+//   return { pageProps };
+// }
+// render() {
+// const { Component, pageProps } = this.props;
+//     return <Component  />;
+//   }
+// }
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
 export default MyApp;

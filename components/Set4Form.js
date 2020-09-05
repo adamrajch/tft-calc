7f151f32e2536e4c221aa6e41334c634ed5a47c1
adamrajch <arajchwald@gmail.com>
2020-09-05 18:40:56 -0500

    set4 json

--- components/Set4Form.js
+++ components/Set4Form.js
@@ -16,12 +16,25 @@ import {
 
 import { useState, useEffect } from "react";
 export default function Set3Form() {
+  const [level, setLevel] = useState("1");
+  function handleChange(e) {
+    if (isNaN(parseInt(e.target.value))) {
+      console.log("value ", e.target.value);
+    } else {
+      const daba = parseInt(e.target.value, 10);
+      if (daba % 10 === 0) {
+        return;
+      }
+      setVal(daba % 10);
+    }
+  }
   return (
     <Grid textAlign="center" columns={2} container stackable inverted>
       <Grid.Row>
         <Message>
           <Message.Header>Coming soon</Message.Header>
         </Message>
+        <label>Test</label> <input onChange={handleChange} value={level} />
       </Grid.Row>
     </Grid>
   );
